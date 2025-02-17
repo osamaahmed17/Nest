991bda7b1979732b17a08d21e359ce66e02feb39
osamaahmed17 <osamaahmedtahir17@gmail.com>
2025-02-17 17:37:56 +0500

    check

--- schema/tests/chapter_test.py
+++ schema/tests/chapter_test.py
@@ -53,8 +53,7 @@ def test_positive(chapter_schema):
         ("sponsors-name-undefined.yaml", "'name' is a required property"),
         ("sponsors-undefined.yaml", "'url' is a required property"),
         ("website-null.yaml", "None is not of type 'string'"),
-
-        #Added test cases for mailing-list validation
+        # Added test cases for mailing-list validation
         ("mailing-list-empty.yaml", "'' is not a 'uri'"),
         ("mailing-list-invalid.yaml", "'https://xyz' is not a 'uri'"),
         ("mailing-list-null.yaml", "None is not a 'uri'"),
